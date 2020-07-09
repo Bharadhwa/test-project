5a737db6b443c59ae9ccce90fcb93748df3149f0
Bharadhwa <68009440+Bharadhwa@users.noreply.github.com>
2020-07-09 15:26:26 +0530

    Update revString.java

--- IdeaProjects/algorithms/src/revString.java
+++ IdeaProjects/algorithms/src/revString.java
@@ -1,4 +1,18 @@
 package PACKAGE_NAME;
 
-public class revString {
+  public class revString
+{
+    public static void main(String[]args)
+    {
+          String str="ABCD";
+          String rev="";
+          int len=str.length();
+          for(int i=len-1;i>=0;i--)
+          {
+              rev=rev+str.charAt(i);
+          }
+          System.out.println("Reverse of the string is :"+ rev);
+    }
+}
+
 }
